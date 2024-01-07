bb845fbebac69f207ca78d8fd919f09d047a997a
Gergul <coolcn@qq.com>
2024-01-07 20:46:54 +0800

    add test

--- README.md
+++ README.md
@@ -7,3 +7,29 @@ quickjs(https://github.com/mensong/quickjs-win) 在windows上的封装
 2.代码包含QJS.h
 
 3.使用qjs.XXX就可以内嵌js引擎了
+
+# 示例
+
+```
+RuntimeHandle rt = qjs.NewRuntime();
+ContextHandle ctx = qjs.NewContext(rt);
+
+ValueHandle result = qjs.RunScript(ctx, qjs.UnicodeToUtf8(L"var a=123;a"), qjs.TheJsNull());
+if (!qjs.JsValueIsException(result))
+{
+	const char* sz = qjs.JsValueToString(ctx, result);
+	printf("运行成功:%s\n", sz);
+	qjs.FreeJsValueToStringBuffer(ctx, sz);
+}
+else
+{
+	ValueHandle exception = qjs.GetAndClearJsLastException(ctx);
+	const char* sz = qjs.JsValueToString(ctx, exception);
+	printf("运行错误:%s\n", sz);
+	qjs.FreeJsValueToStringBuffer(ctx, sz);
+}
+
+qjs.FreeContext(ctx);
+qjs.FreeRuntime(rt);
+```
+
---
+++ Test/Test.cpp
@@ -0,0 +1,31 @@
+﻿// Test.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <stdio.h>
+#include "..\QJS\QJS.h"
+
+int main()
+{
+	RuntimeHandle rt = qjs.NewRuntime();
+	ContextHandle ctx = qjs.NewContext(rt);
+
+	ValueHandle result = qjs.RunScript(ctx, qjs.UnicodeToUtf8(L"var a=123;a"), qjs.TheJsNull());
+	if (!qjs.JsValueIsException(result))
+	{
+		const char* sz = qjs.JsValueToString(ctx, result);
+		printf("运行成功:%s\n", sz);
+		qjs.FreeJsValueToStringBuffer(ctx, sz);
+	}
+	else
+	{
+		ValueHandle exception = qjs.GetAndClearJsLastException(ctx);
+		const char* sz = qjs.JsValueToString(ctx, exception);
+		printf("运行错误:%s\n", sz);
+		qjs.FreeJsValueToStringBuffer(ctx, sz);
+	}
+
+	qjs.FreeContext(ctx);
+	qjs.FreeRuntime(rt);
+
+	return 0;
+}
